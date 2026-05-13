f03fc862d81655eaaf894ec7435026b3cde66801
William Joel Sandoval Casas <sandoval.williamj@gmail.com>
2026-05-13 10:42:22 +0200

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,19 @@
+class MinStack:
+    def __init__(self):
+        self.my_stack = []
+
+    def push(self, val: int) -> None:
+        self.my_stack.append(val)
+
+    def pop(self) -> None:
+        self.my_stack.pop()
+
+    def top(self) -> int:
+        return self.my_stack[len(self.my_stack) - 1]
+
+    def getMin(self) -> int:
+        mini = self.my_stack[0]
+        for i in self.my_stack:
+            mini = min(i, mini)
+        return mini
+        
